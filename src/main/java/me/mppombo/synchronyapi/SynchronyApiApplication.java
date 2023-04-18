56b1cd715f550a98eb216813de0ccd7d93529525
Matthew Pombo <mppombo5@gmail.com>
2023-04-17 23:50:31 -0700

    First commit for Synchrony Project
    - created spring boot template with h2, jpa support from start.spring.io

---
+++ src/main/java/me/mppombo/synchronyapi/SynchronyApiApplication.java
@@ -0,0 +1,13 @@
+package me.mppombo.synchronyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mppombo/synchronyapi/SynchronyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mppombo.synchronyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
