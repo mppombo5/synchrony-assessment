eb2df3eae5fa14a8588997eefd2399fabb266593
Matthew Pombo <mppombo5@gmail.com>
2023-04-20 01:28:49 -0700

    Created ApiUser controller
    - currently has 3 endpoints:
      - /register
        - make a new user
      - /users
        - get information on all registered users
      - /users/{id}
        - get information on a specific user by ID

---
+++ src/main/java/me/mppombo/synchronyapi/controller/ApiUserController.java
@@ -0,0 +1,43 @@
+package me.mppombo.synchronyapi.controller;
+
+import me.mppombo.synchronyapi.domain.ApiUser;
+import me.mppombo.synchronyapi.service.ApiUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ApiUserController {
+    private final static Logger logger = LoggerFactory.getLogger(ApiUserController.class);
+    private final ApiUserService service;
+
+    public ApiUserController(ApiUserService service) {
+        this.service = service;
+    }
+
+
+    // Aggregate all registered users
+    @GetMapping("/users")
+    List<ApiUser> getAllUsers() {
+        logger.info("Processing request for aggregate information of all users");
+        return service.getAllRegisteredUsers();
+    }
+
+    // Get a single user by ID
+    // TODO: add searching function by username/last name
+    @GetMapping("/users/{id}")
+    ApiUser getOneUser(@PathVariable Long id) {
+        logger.info(String.format("Processing request for user data with ID %d", id));
+        return service.getSingleUser(id);
+    }
+
+    // Register new user
+    @PostMapping("/register")
+    ApiUser createUser(@RequestBody ApiUser newUser) {
+        logger.info(String.format("Request to register new user: %s", newUser.toString()));
+        return service.registerNewUser(newUser);
+    }
+}
